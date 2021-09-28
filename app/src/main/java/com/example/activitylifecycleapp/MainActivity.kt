04847392ac34f7439147f8c7cb2a1ebb3e01866d
d7d7s <d7d7sultan@gmail.com>
2021-09-28 13:34:09 +0300

    Activity Lifecycle App boot-camp

---
+++ app/src/main/java/com/example/activitylifecycleapp/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.activitylifecycleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import java.io.Console
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        println("onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        println("onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("noPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("onDestroy")
+    }
+}
